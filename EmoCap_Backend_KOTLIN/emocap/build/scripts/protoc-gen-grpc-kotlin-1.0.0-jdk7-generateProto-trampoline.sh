b19423a559afa989be4ba3fee8d1c4e85122abb3
박성한 <92065248+park56@users.noreply.github.com>
2023-10-31 16:00:41 +0900

    Delete EmoCap_Backend_KOTLIN/emocap/build/scripts directory

--- EmoCap_Backend_KOTLIN/emocap/build/scripts/protoc-gen-grpc-kotlin-1.0.0-jdk7-generateProto-trampoline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec '/Library/Java/JavaVirtualMachines/jdk-18.0.2.1.jdk/Contents/Home/bin/java' -jar '/Users/sunghan/.gradle/caches/modules-2/files-2.1/io.grpc/protoc-gen-grpc-kotlin/1.0.0/d6c8cbe29c60b202165593be5244ce43d37378ac/protoc-gen-grpc-kotlin-1.0.0-jdk7.jar' "$@"
